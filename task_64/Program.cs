715022e3c027ba76d2da610ab26e460ceba9a19d
eribiam <boss.vede@mail.ru>
2022-09-08 02:02:48 +0700

    tast_64

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string IntervalOfNumbers (int start, int stop)
+{
+    if (start == stop) return start.ToString();
+    return (start + ", " + IntervalOfNumbers(start + 1, stop));
+}
+
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (secondNumber > firstNumber)
+{
+    Console.WriteLine($"[ {IntervalOfNumbers(firstNumber, secondNumber)} ]");
+}
+else Console.WriteLine("Второе число должно быть больше первого");
