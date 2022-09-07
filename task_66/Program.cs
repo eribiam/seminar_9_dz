d0d86743c8057d8040e3bb5de5bb401323b7811d
eribiam <boss.vede@mail.ru>
2022-09-08 02:15:53 +0700

    task_66

--- task_66/Program.cs
+++ task_66/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumOfNumbers (int start, int stop)
+{
+    int resultSum = start;
+    if (start == stop) return resultSum;
+    return (resultSum + SumOfNumbers(start + 1, stop));
+}
+
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (secondNumber > firstNumber)
+{
+    Console.Write($"Сумма чисел от {firstNumber} до {secondNumber} = ");
+    Console.WriteLine(SumOfNumbers(firstNumber, secondNumber));
+}
+else Console.WriteLine("Второе число должно быть больше первого");
